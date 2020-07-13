8311d1eef3fe013f1e638e7b287d091424be1e1f
Bharath <albharath.1305@gmail.com>
2020-07-13 14:58:24 +0530

    Decent start

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+
+const App = () => {
+    return (
+        <Typography variant="h1">
+            A template for a react app using material-ui
+        </Typography>
+    );
+};
+
+export default App;
